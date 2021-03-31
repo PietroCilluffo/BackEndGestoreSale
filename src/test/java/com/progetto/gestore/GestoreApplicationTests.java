b48d19d0a35a8e6461745fe6451e4929e8009ed9
Pietro <s257805@studenti.polito.it>
2021-03-31 14:54:08 +0200

    hjaijras

---
+++ src/test/java/com/progetto/gestore/GestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progetto.gestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
